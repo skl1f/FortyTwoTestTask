c38e3a324ca307c84e1f825aa29289142d28e588
Oleksii Miroshnychenko <skl1f@ukrgadget.com>
2015-04-22 17:51:07 +0300

    ticket:3 add hello_base template

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -4,5 +4,6 @@ from .models import Contact
 
 def home(request):
     contact = Contact.objects.get(id=1)
-    return render(request, 'index.html', {'contact': contact},
+    return render(request, 'index.html', {'contact': contact,
+                                          'request': request},
                   content_type="text/html")
