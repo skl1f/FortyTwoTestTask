e28ec808cdc4a115e65a9e81112c0037eacbf963
Oleksii Miroshnychenko <skl1f@ukrgadget.com>
2015-04-22 16:18:42 +0300

    ticket:1 change in tests

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,10 +1,27 @@
 from django.test import TestCase
 from .models import Contact
+from django.test.client import Client
+
+
+class BasicTest(TestCase):
+
+    "Check admin login and password"
+
+    def chek_login(self):
+        c = Client()
+        login = c.login(username='admin', password='admin')
+        assert(login)
 
 
 class ContactTest(TestCase):
+
     def check_contact_info(self):
-        "Check if information is correct"
+
+        "Check that status_code is 200 and information in db is correct"
+
+        c = Client()
+        page = c.get('/')
+        assert(page.status_code == 200)
         contact = Contact.objects.get(id=1)
         assert(contact.name == u'Oleksii')
         assert(contact.lastname == u'Miroshnychenko')
