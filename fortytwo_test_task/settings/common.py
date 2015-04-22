c3df55ca3cd2dad5e8cd1457ed4115ee6fc7148c
Oleksii Miroshnychenko <skl1f@ukrgadget.com>
2015-04-22 12:43:53 +0300

    ticket:1 static files

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -100,14 +100,14 @@ MEDIA_URL = '/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_DIR, "static"),
     os.path.join(BASE_DIR, 'apps', 'hello', 'static'),
 )
 # Template Settings
