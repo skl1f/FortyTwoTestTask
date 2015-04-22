fb194ed4825ae455512067cbc20ad374f843aa49
Oleksii Miroshnychenko <skl1f@ukrgadget.com>
2015-04-22 16:49:19 +0300

    ticket:1 change in fixtures and tests

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -3,20 +3,9 @@ from .models import Contact
 from django.test.client import Client
 
 
-class BasicTest(TestCase):
-
-    "Check admin login and password"
-
-    def chek_login(self):
-        c = Client()
-        login = c.login(username='admin', password='admin')
-        assert(login)
-
-
 class ContactTest(TestCase):
 
     def check_contact_info(self):
-
         "Check that status_code is 200 and information in db is correct"
 
         c = Client()
@@ -27,3 +16,19 @@ class ContactTest(TestCase):
         assert(contact.lastname == u'Miroshnychenko')
         assert(contact.bio == u'Python Dev')
         assert(contact.other_contact == u'https://github.com/skl1f')
+
+    def runTest(self):
+        self.check_contact_info()
+
+
+class CheckAdmin(TestCase):
+
+    def check_login(self):
+        "Check admin default login and password"
+
+        c = Client()
+        login = c.login(username='admin', password='admin')
+        assert(login)
+
+    def runTest(self):
+        self.check_login()
