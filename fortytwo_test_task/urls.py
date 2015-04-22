6b2195a02e07079fe56bb3f1e9ef4dbd3b71ddb9
Oleksii Miroshnychenko <skl1f@ukrgadget.com>
2015-04-22 10:51:30 +0300

    ticket:1 fix static files #6

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,4 +10,4 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
