7c013499e787d0d20ffe1c9e6f99c14efd077e54
Oleksii Miroshnychenko <skl1f@ukrgadget.com>
2015-04-23 13:23:35 +0300

    ticket:3 rewrite middleware

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -19,6 +19,15 @@ class RequestLog(models.Model):
 
     """This object contain iformation about HttpRequest"""
 
+    def save(self, arg):
+        self.full_path = arg['FULL_PATH']
+        self.request_method = arg['REQUEST_METHOD']
+        self.remote_addr = arg['REMOTE_ADDR']
+        self.http_user_agent = arg['HTTP_USER_AGENT']
+        self.http_referer = arg['HTTP_REFERER']
+        self.http_accept_language = arg['HTTP_ACCEPT_LANGUAGE']
+        super(RequestLog, self).save(arg)
+
     full_path = models.CharField(max_length=200)
     request_method = models.CharField(max_length=7)
     remote_addr = models.GenericIPAddressField()
--- apps/hello/utils.py
+++ apps/hello/utils.py
@@ -1,22 +1,40 @@
 from .models import RequestLog, RequestCounter
 
 
+def return_on_failure(value):
+    def decorate(f):
+        def applicator(*args, **kwargs):
+            try:
+                f(*args, **kwargs)
+            except:
+                print('Error')
+
+        return applicator
+
+
 class RequestLoggingMiddleware(object):
+    META_FIELDS = [
+        'REQUEST_METHOD',
+        'REMOTE_ADDR',
+        'HTTP_USER_AGENT',
+        'HTTP_REFERER',
+        'HTTP_ACCEPT_LANGUAGE']
+
+    arg = {'FULL_PATH': '',
+           'REQUEST_METHOD': '',
+           'REMOTE_ADDR': '',
+           'HTTP_USER_AGENT': '',
+           'HTTP_REFERER': '',
+           'HTTP_ACCEPT_LANGUAGE': ''}
 
     def process_request(self, request):
         """
         Saving some of fields from HttpRequest.
         """
-        count = RequestCounter.objects.get(id=1)
-        log = RequestLog()
-        log.full_path = request.get_full_path()
-        log.request_method = request.META['REQUEST_METHOD']
-        log.remote_addr = request.META['REMOTE_ADDR']
-        log.http_user_agent = request.META['HTTP_USER_AGENT']
-        try:
-            log.http_referer = request.META['HTTP_REFERER']
-        except:
-            pass
-        log.http_accept_language = request.META['HTTP_ACCEPT_LANGUAGE']
-        log.save()
-        count.increment()
+        self.arg['FULL_PATH'] = request.get_full_path()
+
+        for field in self.META_FIELDS:
+            if field in request.META:
+                self.arg[field] = request.META[field]
+        RequestLog().save(self.arg)
+        RequestCounter.objects.get(id=1).increment()
